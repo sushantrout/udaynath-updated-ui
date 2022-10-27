d72d413532b27dd4de2fe8bd615e2e6f300ada41
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-10-28 01:04:11 +0530

    Completed

--- un-exam-client/src/app/shared/grade-util.ts
+++ un-exam-client/src/app/shared/grade-util.ts
@@ -25,7 +25,31 @@ export class GradeUtil {
     }
   }
 
-  creditPoint() {
-    return 0;
+  creditPoint(paperResult : any, studentResult : any) {
+    let internalMark = +studentResult[1];
+    let semMark = +studentResult[2];
+    let totalMark = internalMark + semMark;
+    let GP = 0;
+    let cp = paperResult.cp;
+    if(isNaN(internalMark) || isNaN(semMark) || semMark < 24) {
+      GP = 0;
+    } else {
+      if (totalMark >= 90) {
+        GP = 10 * cp;
+      } else if (totalMark >= 80) {
+        GP = 9 * cp;
+      } else if (totalMark >= 70) {
+        GP = 8 * cp;
+      } else if (totalMark >= 60) {
+        GP = 7 * cp;
+      } else if (totalMark >= 50) {
+        GP = 6 * cp;
+      } else if (totalMark >= 45) {
+        GP = 5 * cp;
+      } else if (totalMark >= 40) {
+        GP = 4 * cp;
+      }
+    }
+    return GP;
   }
 }
