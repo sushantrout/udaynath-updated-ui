b8ad81fd626e90163588d899a626cfbc661b720c
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-11-05 10:01:19 +0530

    df

--- un-exam-client/src/app/shared/components/bip-calendar/bip-calendar.component.ts
+++ un-exam-client/src/app/shared/components/bip-calendar/bip-calendar.component.ts
@@ -7,25 +7,33 @@ import { ValueAccessor } from '../bip-shared/bip-value-accessor';
   templateUrl: './bip-calendar.component.html',
   styleUrls: ['./bip-calendar.component.css'],
   providers: [
-    { provide: NG_VALUE_ACCESSOR, useExisting: BipCalendarComponent, multi: true }
-  ]
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: BipCalendarComponent,
+      multi: true,
+    },
+  ],
 })
 export class BipCalendarComponent extends ValueAccessor implements OnInit {
-
-  @Input('showTime') showTime !: boolean;
-  @Input('showSeconds') showSeconds !: boolean;
-  @Input('showIcon') showIcon !: boolean;
-  @Input('placeholder') placeholder !: string;
-  @Input('appendTo') appendTo !:any;
-  @Input('disabled') disabled !:boolean;
+  @Input('showTime') showTime!: boolean;
+  @Input('showSeconds') showSeconds!: boolean;
+  @Input('showIcon') showIcon!: boolean;
+  @Input('placeholder') placeholder!: string;
+  @Input('appendTo') appendTo!: any;
+  @Input('disabled') disabled!: boolean;
+  @Input('styleClass') styleClass!: string;
   constructor() {
     super();
   }
 
+  maxDate = new Date();
+  yearRange = "2000:"+(this.maxDate.getFullYear() + 1)
   ngOnInit(): void {
+    this.maxDate = new Date();
   }
   changeEvent(event: any) {
-    this.changed.forEach((f) => { f(this.value) });
+    this.changed.forEach((f) => {
+      f(this.value);
+    });
   }
-
 }
